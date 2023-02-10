a29d8d962049b762fbacd3881350e9db1a7b7120
vickykoseli <viktoriia.dniestrova@outlook.com>
2023-02-10 17:09:19 -0500

    updates city

--- src/app.js
+++ src/app.js
@@ -87,4 +87,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Istanbul");
